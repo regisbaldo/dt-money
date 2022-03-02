e09b76a5a14a05ab1407d0b270a24fb3f0a11182
Reginaldo <reginaldo.bonaldi@nubbi.com.br>
2022-03-02 09:02:15 -0300

    arrumando cabeçalho da tabela e configurando o miragejs

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -1,12 +1,24 @@
 import { Container } from "./styles";
-
+import { useEffect } from "react";
 export function TransactionsTable() {
+
+    useEffect(() => {
+        fetch("http://localhost:3000/api/transactions")
+            .then((response) => response.json())
+            .then((response) => {
+                console.log(response);
+            })
+
+    }, [])
+
     return <Container>
         <table>
             <thead>
-                <th>1</th>
-                <th>2</th>
-                <th>3</th>
+                <tr>
+                    <th>1</th>
+                    <th>2</th>
+                    <th>3</th>
+                </tr>
             </thead>
             <tbody>
                 <tr>
--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,31 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { GlobalStyle } from './assets/styles/global';
+import { createServer } from 'miragejs';
+
+createServer({
+  routes() {
+    this.namespace = "api";
+    this.get("/transactions", () => {
+      return [
+        {
+          id: 1
+        },
+        {
+          id: 2
+        }
+      ]
+    })
+  }
+});
+
+
+
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
-    <GlobalStyle/>
+    <GlobalStyle />
   </React.StrictMode>,
   document.getElementById('root')
 );
